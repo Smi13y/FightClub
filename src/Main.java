1f58af50f647771f933053e417af05e3b31c1a15
Smi13y <anthony.w.langley@gmail.com>
2022-04-02 20:26:25 -0400

    version 0.04 ============ -Runs!
    Upcoming features:
    -Create rosters
    -save rosters
    -save w/d/l records
    -automatic tournaments
    -

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Character ryu = new Character("Ryu", "Balance", 3, 3, 3, 3, 50);
+        Character ken = new Character("Ken", "Balance", 3, 3, 3, 3, 50);
+
+        Battle.fight(ryu,ken);
+    }
+}
